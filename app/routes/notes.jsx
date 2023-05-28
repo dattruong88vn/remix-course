090305117c6785bee66b5506412037bb90f4ca95
dattruong88vn <dattt1.ho@mcredit.com.vn>
2023-05-28 09:44:21 +0700

    20. Adding backend code and redirecting

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
+import { redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
   return (
@@ -6,6 +8,23 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+export async function action({ request }) {
+  // get data
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+
+  // add validation
+
+  // save data
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storeNotes(updatedNotes);
+
+  // redirect
+  return redirect("/notes");
 }
 
 export function links() {
